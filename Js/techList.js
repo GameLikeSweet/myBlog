3d7c557235193df41d771fc8ac70b0ed407c20ae
tjdgns9978 <tjdgns9978@gmail.com>
2025-05-30 18:07:02 +0900

    techList sorting

--- Js/techList.js
+++ Js/techList.js
@@ -13,41 +13,52 @@ function toggleLib() {
 
 function addTechNote() {
     $.getJSON('/Json/techNoteList.json', function (data) {
-        $.each(data, function (key, items) {
-            if (key === 'web' || key === 'git' || key === 'lib') {
-                makingTechNote(items, key);
-            }
-            // page 등 기타는 무시(추가하지 않음)
+        let allList = [];
+
+        // 각 그룹(web, git, lib)에서 숫자 key만 배열에 모으기
+        ['web', 'git', 'lib'].forEach(function(type) {
+            let items = data[type];
+            if (!items) return;
+            Object.keys(items).forEach(function(key) {
+                if (!isNaN(Number(key))) {
+                    let obj = items[key];
+                    allList.push({
+                        idx: Number(key),  // 숫자 key
+                        type: type,        // 카테고리(web, git, lib)
+                        obj: obj           // 실제 데이터
+                    });
+                }
+            });
+        });
+
+        // 한 번에 오름차순 정렬
+        allList.sort(function(a, b) {
+            return a.idx - b.idx;
+        });
+
+        // 순서대로 DOM 추가
+        allList.forEach(function(item) {
+            let obj = item.obj;
+            if (!obj || !obj.title || !obj.url) return;
+
+            let $techBox = $('<div>').addClass(item.type + ' selected');
+            let $a = $('<a>').attr('href', obj.url);
+            let $inner = $('<div>').addClass('inner');
+
+            let $techImg = $('<div>').addClass('techImg');
+            if (obj.img) $techImg.css('background-image', 'url(' + obj.img + ')');
+
+            let $techAbout = $('<div>').addClass('techAbout');
+            let $title = $('<div>').addClass('techTitle').text(obj.title);
+            let $explain = $('<div>').addClass('techExplain').text(obj.explain || "");
+
+            $techAbout.append($title, $explain);
+            $inner.append($techImg, $techAbout);
+            $a.append($inner);
+            $techBox.append($a);
+
+            $('.techBot').append($techBox);
         });
     });
 }
 
-function makingTechNote(items, key) {
-    // 숫자 키만 오름차순 정렬
-    let keys = Object.keys(items)
-        .filter(numKey => !isNaN(Number(numKey))) // 숫자 키만
-        .sort((a, b) => Number(a) - Number(b));   // 오름차순
-
-    $.each(keys, function (idx, numKey) {
-        let obj = items[numKey];
-        if (!obj || !obj.title || !obj.url) return;
-
-        let $techBox = $('<div>').addClass(key + ' selected');
-        let $a = $('<a>').attr('href', obj.url);
-        let $inner = $('<div>').addClass('inner');
-
-        let $techImg = $('<div>').addClass('techImg');
-        if (obj.img) $techImg.css('background-image', 'url(' + obj.img + ')');
-
-        let $techAbout = $('<div>').addClass('techAbout');
-        let $title = $('<div>').addClass('techTitle').text(obj.title);
-        let $explain = $('<div>').addClass('techExplain').text(obj.explain || "isEmpty");
-
-        $techAbout.append($title, $explain);
-        $inner.append($techImg, $techAbout);
-        $a.append($inner);
-        $techBox.append($a);
-
-        $('.techBot').append($techBox);
-    });
-}
